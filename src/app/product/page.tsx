6a2c9f835980a754a3cc2195b07b894f05046a55
Tibet Teixeira <tibetbrasil@gmail.com>
2025-02-19 00:39:48 -0300

    feat: add shadcn inital setup and components

---
+++ src/app/product/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <div className="bg-neutral-700 w-full h-full flex flex-col items-center justify-center">
+      <h1 className="text-neutral-100">Product Page</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default ProductPage;
