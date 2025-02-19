cfa6813a0078eebd99097c5ca33396fb85e4f152
Tibet Teixeira <tibetbrasil@gmail.com>
2025-02-19 01:02:49 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 const ProductPage = () => {
   return (
-    <div className="bg-neutral-700 w-full h-full flex flex-col items-center justify-center">
+    <div className="flex h-full w-full flex-col items-center justify-center bg-neutral-700">
       <h1 className="text-neutral-100">Product Page</h1>
       <Button>Click me</Button>
     </div>
