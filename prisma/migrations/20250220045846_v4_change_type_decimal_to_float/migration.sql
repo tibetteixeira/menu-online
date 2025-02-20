53002606dd3cecdcf4bd764969b4303db83cd760
Tibet Teixeira <tibetbrasil@gmail.com>
2025-02-20 02:54:19 -0300

    feat: change type decimal to float

---
+++ prisma/migrations/20250220045846_v4_change_type_decimal_to_float/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `total` on the `Order` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `DoublePrecision`.
+  - You are about to alter the column `price` on the `OrderItem` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `DoublePrecision`.
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `DoublePrecision`.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" ALTER COLUMN "total" SET DATA TYPE DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "OrderItem" ALTER COLUMN "price" SET DATA TYPE DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "Product" ALTER COLUMN "price" SET DATA TYPE DOUBLE PRECISION;
