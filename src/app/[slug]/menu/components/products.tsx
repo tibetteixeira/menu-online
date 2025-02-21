6141fc644d6e09d4a094c6da5105a152dd68e3ae
Tibet Teixeira <tibetbrasil@gmail.com>
2025-02-21 00:44:26 -0300

    feat: create format currency function

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { formatCurrency } from "@/app/helpers/format-currency";
+
 interface ProductsProps {
   products: Product[];
 }
@@ -24,10 +26,7 @@ const Products = ({ products }: ProductsProps) => {
               {product.description}
             </p>
             <p className="pt-3 text-sm font-semibold">
-              {Intl.NumberFormat("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }).format(product.price)}
+              {formatCurrency(product.price)}
             </p>
           </div>
           <div className="relative min-h-[120px] min-w-[120px]">
