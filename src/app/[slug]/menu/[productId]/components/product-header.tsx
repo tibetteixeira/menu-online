d994b91a651589588aece2d75e5702d09f969487
Tibet Teixeira <tibetbrasil@gmail.com>
2025-02-24 02:17:57 -0300

    feat: it creates buttons header component

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -1,45 +1,15 @@
 "use client";
 
 import { Product } from "@prisma/client";
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
+import ButtonsHeader from "../../components/buttons-header";
 
 interface ProductHeaderProps {
   product: Pick<Product, "imageUrl" | "name">;
 }
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
-  const router = useRouter();
-  const handleBack = () => router.back();
-
-  return (
-    <div className="relative min-h-[300px] w-full">
-      <Button
-        variant="secondary"
-        size="icon"
-        onClick={handleBack}
-        className="absolute left-4 top-4 z-10 rounded-full"
-      >
-        <ChevronLeftIcon />
-      </Button>
-      <Image
-        src={product.imageUrl}
-        fill
-        alt={product.name}
-        className="object-contain"
-      />
-      <Button
-        variant="secondary"
-        size="icon"
-        className="absolute right-4 top-4 z-10 rounded-full"
-      >
-        <ScrollTextIcon />
-      </Button>
-    </div>
-  );
+  return <ButtonsHeader alt={product.name} imageUrl={product.imageUrl} imageStyle="object-contain" />;
 };
 
 export default ProductHeader;
---
+++ src/app/[slug]/menu/components/buttons-header.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+interface RestaurantHeaderProps {
+  alt?: string;
+  imageUrl?: string;
+  imageStyle?: string;
+  onlyBackButton?: boolean;
+}
+
+const ButtonsHeader = ({
+  alt,
+  imageUrl,
+  imageStyle,
+  onlyBackButton,
+}: RestaurantHeaderProps) => {
+  const router = useRouter();
+  const handleBack = () => router.back();
+
+  if (onlyBackButton) {
+    return (
+      <div className="relative w-full">
+        <Button
+          variant="secondary"
+          size="icon"
+          className="absolute left-4 top-4 z-10 rounded-full"
+          onClick={handleBack}
+        >
+          <ChevronLeftIcon />
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative min-h-[280px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-10 rounded-full"
+        onClick={handleBack}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      {imageUrl && alt && (
+        <Image src={imageUrl} fill alt={alt} className={`${imageStyle}`} />
+      )}
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-10 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default ButtonsHeader;
